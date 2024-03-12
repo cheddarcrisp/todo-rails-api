eb6d4c383647caa041d3ccbc0433b25a20465dfc
CheddarCrisp <cheddar@crispbyte.dev>
2024-03-12 16:15:16 -0400

    add completed flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_200216) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_201315) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
