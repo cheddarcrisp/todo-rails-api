6ef9ba1f4f88740f5b1c53865c6e4d4886121f36
CheddarCrisp <cheddar@crispbyte.dev>
2024-03-27 10:49:02 -0400

    extract service code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,54 +1,41 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show update destroy ]
-
   # GET /todos
   def index
     @todos = Todo.all
 
-    render json: @todos
+    render json: TodosService.get_all
   end
 
   # GET /todos/1
   def show
-    render json: @todo
+    render json: TodosService.get(id: params[:id])
   end
 
   # POST /todos
   def create
-    @todo = Todo.new(todo_params)
-
-    if @todo.save
-      render json: @todo, status: :created, location: @todo
-    else
-      render json: @todo.errors, status: :unprocessable_entity
-    end
+    todo = Todo.create(todo_params)
+    render json: todo, status: :created, location: todo
   end
 
   # PATCH/PUT /todos/1
   def update
-    if @todo.update(todo_params)
-      render json: @todo
-    else
-      render json: @todo.errors, status: :unprocessable_entity
-    end
+    render json: TodosService.update(
+      id: params[:id],
+      title: todo_params[:title],
+      completed: todo_params[:completed],
+      order: todo_params[:order])
   end
 
   # DELETE /todos/1
   def destroy
-    @todo.destroy!
+    TodosService.delete(id: params[:id])
   end
 
   def destroy_all
-    Todo.destroy_all
+    TodosService.delete_all
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.require(:todo).permit(:title, :completed, :order)
     end
---
+++ app/services/todos_service.rb
@@ -0,0 +1,27 @@
+class TodosService
+  class << self
+    def get_all
+      Todo.all
+    end
+
+    def get(id:)
+      Todo.find(id)
+    end
+
+    def create(title:, order:)
+      Todo.create(title, order)
+    end
+
+    def update(id:, title:, completed:, order:)
+      Todo.update(id, title: title, completed: completed, order: order)
+    end
+
+    def delete(id:)
+      Todo.destroy(id)
+    end
+
+    def delete_all
+      Todo.destroy_all
+    end
+  end
+end
